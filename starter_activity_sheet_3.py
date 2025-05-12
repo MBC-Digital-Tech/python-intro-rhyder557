e39a5d7445fcb4321f07073661e390b1773201b9
Lee Macri <lee.macri@mbc.school.nz>
2025-05-13 11:53:48 +1200

    Update starter_activity_sheet_3.py

--- starter_activity_sheet_3.py
+++ starter_activity_sheet_3.py
@@ -1,15 +1,20 @@
 # starter program week 3 - perhaps this could be a task
 
-
 def cooking():
     print("Meal planner")
     print()
+
+    # change these to your favourite meals
     print("1. Chicken curry ")
     print("2. Veggie lasagne")
     print("3. Burger and salad")
+    # add one more meal here
     print()
+    # adapt the next line to add in the 4.
     print("Which of these meals is your favourite? (1, 2 or 3) ")
+    # combine the line above and below into one command that accepts an integer instead of a string.
     answer = input()
+    # adapt the if else block to include the fourth meal and compares integers instead of strings.
     if answer == "1":
         print("Chicken curry coming up")
     elif answer == "2":
@@ -18,7 +23,5 @@ def cooking():
         print("Burger and salad coming up!")
     print("Enjoy!")
     
-     
-    
-cooking()
+# add a command to run the function above.
 
