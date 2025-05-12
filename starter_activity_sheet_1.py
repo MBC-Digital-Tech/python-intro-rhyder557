1f4337aa9d7e2519c45e7cab76918c202dc35296
Lee Macri <lee.macri@mbc.school.nz>
2025-05-13 11:03:26 +1200

    Add comments.

--- starter_activity_sheet_1.py
+++ starter_activity_sheet_1.py
@@ -1,11 +1,16 @@
 # starter program lesson 1
 
+# change to your name.
 name = "Sum Gai"
 print("We want to know if you like progamming!")
 print()
+# combine the next 2 lines into one commeand
 print("Do you like programming " + name + "?")
 answer = input()
+
+# change to an f string
 print("Great! You said " + answer + "!")
-print("Let's learn some Python today")
+
+print("Let's learn some Python today.")
 
 
