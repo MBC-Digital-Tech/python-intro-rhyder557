b38213e821f838e7d2bf7e62ddc8756d1273cc99
Lee Macri <lee.macri@mbc.school.nz>
2025-05-13 11:13:54 +1200

    Update starter_activity_sheet_2.py

--- starter_activity_sheet_2.py
+++ starter_activity_sheet_2.py
@@ -3,13 +3,18 @@
 def conversation():
     print("Welcome to my conversation program")
     print()
+
+    # combine the next two lines into one command.
     print("Do you like cycling? Answer yes or no")
     answer = input()
+
+    # chenge this so that the user can enter YES as well.
     if answer == "yes":
         print("That's good - you will get very fit")
     else:
         print("Perhaps you like some other sport. ")
+    
     print("Goodbye")
 
-conversation()
+# Add command here to run the function
 
